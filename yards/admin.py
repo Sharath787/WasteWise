01fc87a0e72573c64db627fe5335cfcbf3c63b7e
Sharath Kumar <shkumar@synamedia.com>
2026-08-09 15:31:00 +0530

    add admin registration to yard and users model

--- yards/admin.py
+++ yards/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import WasteType, Yard, YardWasteCapacity
 
-# Register your models here.
+
+@admin.register(WasteType)
+class WasteTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+
+class YardWasteCapacityInline(admin.TabularInline):
+    model = YardWasteCapacity
+    extra = 1
+
+
+@admin.register(Yard)
+class YardAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'contact_number', 'is_active')
+    list_filter = ('is_active',)
+    inlines = [YardWasteCapacityInline]
